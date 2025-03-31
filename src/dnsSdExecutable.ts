bfb978d8c537210be88b67a74b913799660471ba
Arne Goedeke <arne@deuso.de>
2025-03-31 19:36:40 +0200

    Fix windows executable name

--- src/dnsSdExecutable.ts
+++ src/dnsSdExecutable.ts
@@ -2,7 +2,7 @@
 import { platform } from "node:os";
 import { throwUnsupportedPlatformError } from "./unsupportedPlatformError.js";
 
-export const dnsSdExecutableWin32 = "dns_sd.exe";
+export const dnsSdExecutableWin32 = "dns-sd.exe";
 export const dnsSdExecutableMacOs = "dns-sd";
 
 export function dnsSdExecutable(): string {
